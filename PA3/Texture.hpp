bf8e63d628bdb7bf3b47889f46af77537fd1f4c6
LanFear-Li <1371705749@qq.com>
2022-05-24 23:03:37 +0800

    pa3 partly finished

--- PA3/Texture.hpp
+++ PA3/Texture.hpp
@@ -1,19 +1,17 @@
-//
-// Created by LEI XU on 4/27/19.
-//
-
 #ifndef RASTERIZER_TEXTURE_H
 #define RASTERIZER_TEXTURE_H
+
 #include "global.hpp"
+
 #include <eigen3/Eigen/Eigen>
 #include <opencv2/opencv.hpp>
-class Texture{
+
+class Texture {
 private:
     cv::Mat image_data;
 
 public:
-    Texture(const std::string& name)
-    {
+    Texture(const std::string &name) {
         image_data = cv::imread(name);
         cv::cvtColor(image_data, image_data, cv::COLOR_RGB2BGR);
         width = image_data.cols;
@@ -22,8 +20,7 @@ public:
 
     int width, height;
 
-    Eigen::Vector3f getColor(float u, float v)
-    {
+    Eigen::Vector3f getColor(float u, float v) {
         auto u_img = u * width;
         auto v_img = (1 - v) * height;
         auto color = image_data.at<cv::Vec3b>(v_img, u_img);
@@ -31,4 +28,5 @@ public:
     }
 
 };
+
 #endif //RASTERIZER_TEXTURE_H
